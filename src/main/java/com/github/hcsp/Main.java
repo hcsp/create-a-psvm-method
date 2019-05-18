debedc75cd53d2ae2e3c0fe7fc7819a2b7576ecd
Bo Zhang <bo@gradle.com>
2019-05-18 20:19:56 +0800

    More repositories

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,3 @@
+package com.github.hcsp;
+
+public class Main {}
---
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -0,0 +1,10 @@
+package com.github.hcsp;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void importStringUtilsCorrectly() {
+        Main.main(null);
+    }
+}
