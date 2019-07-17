1979dcbcb5a6487af7386e5f0f8bc1b334369c4b
wangpeng1994 <947034046@qq.com>
2019-07-17 13:43:52 +0800

    Create public static method main.

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,16 @@
 package com.github.hcsp;
 
 public class Main {
+    public static int i;
+
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
-
+    public static void main(String[] args) {
+        d();
+        d();
+        d();
+    }
+    public static void d() {
+        i = i + 1;
+    }
 }
