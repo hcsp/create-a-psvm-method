b1a4b9b05e63481dbc2c693b0dcf494aec2d7f3b
uuflyer <49433104+uuflyer@users.noreply.github.com>
2019-07-31 16:17:30 +0800

    创建一个方法 (#58)
    创建一个方法 (#58)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,10 @@
 package com.github.hcsp;
 
 public class Main {
+
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+    public static void main(String[] args) {
+    }
 
 }
