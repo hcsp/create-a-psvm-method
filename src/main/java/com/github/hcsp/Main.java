cca0882c52860ab34a63566c57ee0a61ff458868
w-cao <56790101+w-cao@users.noreply.github.com>
2019-10-25 12:51:48 +0800

    创建一个psvm方法 (#107)
    创建一个psvm方法 (#107)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,15 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+    public static void main(String[] args) {
+        //令x = 0
+        int x = 1;
+        f(x);
 
+        System.out.println(f(x));
+    }
+
+    public static int f(int x) {
+        return x + 1;
+    }
 }
