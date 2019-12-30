cb11fdb8ed5ad20d6950d30bf65a6eeedf2eef33
dygalo <2522486321@qq.com>
2019-12-30 23:19:38 +0800

    创建一个public static void main(String[] args)方法

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,24 @@
 package com.github.hcsp;
 
-public class Main {
-    // Create a public static void main(String[] args) method here.
-    // 在这里创建一个public static void main(String[] args)方法。
 
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_sv;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=0;
+        System.out.println(f(x));
+        int a=2,b=1;
+        f(a,b);
+        System.out.println(f(a,b));
+        g();
+    }
+
+    public static int f(int x) {
+           return x+1;
+    }
+    public static int f(int x,int y){
+           return x-y;
+    }
+    public static void g() {
+    }
 }
