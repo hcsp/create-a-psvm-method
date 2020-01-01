76c40a2966ecbff97bf74df582cb9295c3d02a76
richard1230 <huyongjinnuaa@163.com>
2020-01-01 13:27:40 +0800

    创建一个名为main、参数为String[] args的public static void方法， 并在标准输出Standard Output打印Hello

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,46 +7,47 @@ public class Main {
     // 在这里创建一个public static void main(String[] args)方法。
     //psvm + tab键
     public static void main(String[] args) {
-        int x = 0;
-        System.out.println(f(x));
-        a();
-
-        int a=2,b=1;
-        f(a,b);
-
-        f(1);
-        f(2);
-        f(3);
-
-        add();
-        add();
-        add();
+//        int x = 0;
+//        System.out.println(f(x));
+//        a();
+//
+//        int a=2,b=1;
+//        f(a,b);
+//
+//        f(1);
+//        f(2);
+//        f(3);
+//
+//        add();
+//        add();
+//        add();
+        System.out.println("Hello");
 
     }
 
-    public static void add(){
-        i = i + 1;
-    }
-
-    public static void a(){
-        b();
-    }
-    public static  void b(){
-        c();
-    }
-
-    public static  void c(){
-        System.out.println("你跳到c了");
-    }
-
-    public static int f(int x){
-        int a = 2*x;//a是一个局部变量,其作用域为包含它的最近的那对大括号!
-                    // 局部变量只在函数每次被调用的时候有效,函数调用结束的时候就会被销毁!
-        return x+a;
-    }
-
-    public static int f(int x,int y){
-        return x-y;
-    }
+//    public static void add(){
+//        i = i + 1;
+//    }
+//
+//    public static void a(){
+//        b();
+//    }
+//    public static  void b(){
+//        c();
+//    }
+//
+//    public static  void c(){
+//        System.out.println("你跳到c了");
+//    }
+//
+//    public static int f(int x){
+//        int a = 2*x;//a是一个局部变量,其作用域为包含它的最近的那对大括号!
+//                    // 局部变量只在函数每次被调用的时候有效,函数调用结束的时候就会被销毁!
+//        return x+a;
+//    }
+//
+//    public static int f(int x,int y){
+//        return x-y;
+//    }
 
 }
