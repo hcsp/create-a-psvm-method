6cc963f22ca1f863c7c9c72020cabdff3b6d6b94
Blink <o8688868o@gmail.com>
2020-01-04 02:49:11 +0800

    创建一个public static void main(String[] args)方法 (#144)
    创建一个public static void main(String[] args)方法 (#144)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,8 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+public static void main(String[] args){
+
+}
 
 }
