8a5e250e76f2c808e05896c94bf181a8bcaaf08f
XinleZhou <2320789321>
2020-02-21 20:52:25 +0800

    dgd

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,12 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
-
+    public static void main(String[] args) {
+        int x=0;
+        f(x);
+        System.out.println(f(x));
+    }
+    public static int f(int x){
+        return x+1;
+    }
 }
