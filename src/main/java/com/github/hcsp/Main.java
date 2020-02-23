cec13798d3ea8846a3c3e4e39993c006d5f45202
pathjh <1639925487@qq.com>
2020-02-23 17:59:35 +0800

    创建静态类方法 (#170)
    创建静态类方法 (#170)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,6 +2,9 @@ package com.github.hcsp;
 
 public class Main {
     // Create a public static void main(String[] args) method here.
-    // 在这里创建一个public static void main(String[] args)方法。
+    public static void main(String[] args){
+        System.out.print("Are you Ok?");
+    }
+
 
 }
