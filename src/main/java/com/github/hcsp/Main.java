d351db548b1d48cfcbe2f2d3218b080486e8b71b
yujie-zhang <907420318@qq.com>
2020-03-27 15:03:25 +0800

    Java程序的基本结构练习：方法

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,15 +1,7 @@
 package com.github.hcsp;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 public class Main {
-    // Create a public static void main(String[] args) method here.
-    // 在这里创建一个public static void main(String[] args)方法。
     public static void main(String[] args) {
 
     }
 }
-
-
-
-
