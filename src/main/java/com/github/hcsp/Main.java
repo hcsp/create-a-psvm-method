d457219693eeed67e28d95dd68819cc8f0382117
jumodada <864353360@qq.com>
2020-06-27 16:32:46 +0800

    移除cat

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,10 +3,7 @@ package com.github.hcsp;
 public class Main {
     static int i = 0;
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.name = "black";
-        cat.meow();
-        System.out.println(i);
+
     }
 
 
