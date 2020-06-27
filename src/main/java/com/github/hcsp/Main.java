6b24e20a2cb11a386cb5155d5ed41ac22d9e88cc
wjxalexander <wjxalexander@hotmail.com>
2020-06-27 10:04:39 +0800

    创建public static void main

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,23 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+    public static  int i; // 静态成员变量 会持续存在
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+        System.out.println(add(x,y));
+        System.out.println(minus(x,y,i));
+    }
+    public static int add(int x, int y){
+        i = i+1;// 修改i
+        return  x + y + i;
+    }
+    public static int minus(int x, int y, int i){
+        int a = 4;
+        {
+            System.out.println(a);
+        }
+        return x - y - i;
+    }
 
 }
