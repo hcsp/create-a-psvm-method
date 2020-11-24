8e6d2bd9948bc1a91406f88c2ec2e3596afcd82d
niuweizhe <niu853273494@qq.com>
2020-11-24 18:42:42 +0800

    创建一个名为main、参数为String[] args的public static void方法

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -2,9 +2,11 @@ package com.github.hcsp;
 
 import org.junit.jupiter.api.Test;
 
+import static com.github.hcsp.Main.*;
+
 public class MainTest {
     @Test
     public void importStringUtilsCorrectly() {
-        Main.main(null);
+        main(null);
     }
 }
