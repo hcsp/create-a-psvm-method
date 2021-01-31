bb39ce451801439f038ecc338552c0ceaedfede3
sltap <wuzhenan00@gmail.com>
2021-01-31 22:20:09 +0800

    Create Functions Wu Zhenan

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,17 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
-
+    public static void main(String[] args) {
+        int x=0;
+        System.out.println(f(x));
+        x=2;
+        int y=1;
+        System.out.println(f(x,y));
+    }
+    public static int f(int x){
+        return x+1;
+    }
+    public static int f(int x,int y){
+        return x-y;
+    }
 }
