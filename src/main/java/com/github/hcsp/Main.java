3df43f46015d2c469a05194f29890d996599481e
Hsikua <48636745+Hsikua@users.noreply.github.com>
2021-03-16 13:12:57 +0800

    创建了一个方法 (#259)
    创建了一个方法 (#259)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,18 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(f(x));
+        int a = 2,b = 1;
+        System.out.println(f(a,b));
+    }
 
+    public static int f(int x) {
+        return x + 1;
+    }
+
+    public static int f(int x, int y) {
+        return x - y ;
+    }
 }
