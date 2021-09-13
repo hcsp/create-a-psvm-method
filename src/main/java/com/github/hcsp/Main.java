269ed6cd0d932974152ada9848cfa45e1ab30f88
朱骏杰 <zhujunjie@sundear.com>
2021-09-13 10:41:47 +0800

    第二题完成

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,5 +3,14 @@ package com.github.hcsp;
 public class Main {
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
+    public static int i ;
 
+    public static void main(String[] args) {
+        add();
+        add();
+        add();
+    }
+    public static void add() {
+        i = i + 1;
+    }
 }
