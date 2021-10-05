4aac7a127ef1afda77e82886eb14f9bc6984580a
chenrubin1108 <chenrubin1108@gmail.com>
2021-10-05 17:19:09 +0800

    1

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,6 +1,9 @@
 package com.github.hcsp;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
     // Create a public static void main(String[] args) method here.
     // 在这里创建一个public static void main(String[] args)方法。
 
